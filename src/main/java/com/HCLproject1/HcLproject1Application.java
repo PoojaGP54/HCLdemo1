0ac4ca47fe4231cb57c81c4a48d88a60b9868c1a
Pooja G.P <poojagp.entransys@gmail.com>
2020-10-13 09:48:38 +0530

    Initial commit

---
+++ src/main/java/com/HCLproject1/HcLproject1Application.java
@@ -0,0 +1,13 @@
+package com.HCLproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcLproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcLproject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/HCLproject1/HcLproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HCLproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcLproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
